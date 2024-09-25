85e326283f784312cdba2d079ba0f2d1c0beec0b
Roy Fernandez <u20221b778@upc.edu.pe>
2024-09-25 18:00:11 -0500

    feat(Subscription2): add header and footer

---
+++ src/app/subscription-plans/subscription-plans.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import {HeaderComponent} from "../public/components/header/header.component";
+import {FooterComponent} from "../public/components/footer/footer.component";
+
+@Component({
+  selector: 'app-subscription-plans',
+  standalone: true,  // Declara que este es un componente independiente
+  templateUrl: './subscription-plans.component.html',
+  imports: [
+    HeaderComponent,
+    FooterComponent
+  ],
+  styleUrls: ['./subscription-plans.component.css']
+})
+export class SubscriptionPlansComponent {
+  // Aquí puedes agregar la lógica del componente
+}
