52127eee79cd18f763ab12c202ab2b9c365768a6
cristianipa7 <cristianir007@gmail.com>
2024-09-25 15:35:19 -0500

    built(index.html): adding google font api for icons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import {HeaderComponent} from "./public/components/header/header.component";
 import {FooterComponent} from "./public/components/footer/footer.component";
 
+import { BookDetailComponent } from './book-detail/book-detail.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, BookDetailComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
