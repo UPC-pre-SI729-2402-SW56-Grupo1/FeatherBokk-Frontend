f9879ccd15df3b8ccc3ab97d10331f5cfa0848f3
Roy Fernandez <u20221b778@upc.edu.pe>
2024-09-25 17:34:34 -0500

    feat(Subscription): add Subscription options code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import {HeaderComponent} from "./public/components/header/header.component";
-import {FooterComponent} from "./public/components/footer/footer.component";
+import { SubscriptionPlansComponent } from './subscription-plans/subscription-plans.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [SubscriptionPlansComponent],  // Importa el componente aquí
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'FeatherBook-Frontend';
+  title = 'mi-aplicacion';
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { SubscriptionPlansComponent } from './subscription-plans/subscription-plans.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'subscription-plans', component: SubscriptionPlansComponent },
+  // Otras rutas aquí
+];
