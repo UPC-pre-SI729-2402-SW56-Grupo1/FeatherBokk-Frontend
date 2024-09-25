027224a69624dbdcbbf6dae3b109c438efb552ba
Roy Fernandez <u20221b778@upc.edu.pe>
2024-09-25 18:33:50 -0500

    feat(Profile): add profile dates code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { UserProfileComponent } from './user-profile/user-profile.component';
 import {HeaderComponent} from "./public/components/header/header.component";
 import {FooterComponent} from "./public/components/footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent,UserProfileComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
