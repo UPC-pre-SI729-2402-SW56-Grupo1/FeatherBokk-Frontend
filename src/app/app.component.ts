7dd274874723d81b04a9f2b68dcf41fb6a128e29
cristianipa7 <cristianir007@gmail.com>
2024-09-26 16:07:05 -0500

    feat(router): add router endpoints

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,10 @@ import { RouterOutlet } from '@angular/router';
 import {HeaderComponent} from "./public/components/header/header.component";
 import {FooterComponent} from "./public/components/footer/footer.component";
 
-import { BookDetailComponent } from './book-preview/book-detail/book-detail.component';
-
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, BookDetailComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
